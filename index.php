ab546db76c4f43423967e29677066b07f7f95a0d
CahyaHayyuni <cahyahafidzah1@gmail.com>
2021-04-21 09:38:46 +0800

    oi oi oi

--- index.php
+++ index.php
@@ -197,7 +197,7 @@ if ($_SESSION['admin'] || $_SESSION['user']) {
         </script>
         <!-- CUSTOM SCRIPTS -->
         <script src="assets/js/custom.js"></script>
-        <!-- SCTIPIS SIGNATURE -->
+        <!-- SCRIPT SIGNATURE -->
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.5.0-beta4/html2canvas.min.js"></script>
 
